58c161553c6bddfa56ca0aa085262cd4457d7a17
joaovsa7 <jvsadesign@gmail.com>
2024-05-16 12:12:12 -0300

    chore: add next sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.youtubetomp3.tube',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
